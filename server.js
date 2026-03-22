28fb143c024c86a8f9d9378572c96326c0f8be5a
Indumathi M <indumathimurugan745@gmail.com>
2026-03-22 21:30:53 +0530

    fixed theater routes

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var dotenv = require("dotenv")
 
 var movieRoutes = require("./routes/movieRoutes")
 var bookingRoutes = require("./routes/bookingRoutes")
+var theaterRoutes = require("./routes/theaterRoutes")
 
 dotenv.config()
 
@@ -13,18 +14,20 @@ var app = express()
 app.use(cors())
 app.use(express.json())
 
-app.use("/api/movies", movieRoutes)
-app.use("/api/bookings", bookingRoutes)
-
 app.get("/", function (req, res) {
   res.json({ message: "PopcornPass API is running!" })
 })
 
-var PORT = process.env.PORT || 5000
+app.use("/api/movies", movieRoutes)
+app.use("/api/bookings", bookingRoutes)
+app.use("/api/theaters", theaterRoutes)
 
-mongoose.connect(process.env.MONGO_URI)
+var PORT = process.env.PORT || 5000
+var MONGO_URI = process.env.MONGO_URI
+
+mongoose.connect(MONGO_URI)
   .then(function () {
-    console.log("MongoDB connected")
+    console.log("MongoDB connected!")
     app.listen(PORT, function () {
       console.log("Server running on port " + PORT)
     })
