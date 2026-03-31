49f1210856379bcff43d88f97eb85d69f158a881
Indumathi M <indumathimurugan745@gmail.com>
2026-03-31 11:46:34 +0530

    seatbookingblock

--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -1,6 +1,23 @@
 var express = require("express")
 var router = express.Router()
 var bookingController = require("../controllers/bookingController")
+var Booking = require("../models/Booking")
+
+// Get all bookings for a specific showtime
+router.get("/showtime", function(req, res) {
+  var { date, time, screen } = req.query
+  Booking.find({
+    "showtime.date": date,
+    "showtime.time": time,
+    "showtime.screen": screen
+  })
+    .then(function(bookings) {
+      res.json({ success: true, data: bookings })
+    })
+    .catch(function(err) {
+      res.status(500).json({ success: false, message: err.message })
+    })
+})
 
 router.get("/", bookingController.getAllBookings)
 router.get("/user/:email", bookingController.getBookingsByEmail)
